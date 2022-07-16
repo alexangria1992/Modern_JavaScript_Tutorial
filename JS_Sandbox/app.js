8ebc899240d8f1b8021a7fa0b75d7684dcad8571
alexangria1992 <alex040892@gmail.com>
2022-07-16 18:28:54 +1200

    Using the console

--- JS_Sandbox/app.js
+++ JS_Sandbox/app.js
@@ -1,1 +1,18 @@
-alert("hello");
+// alert("hello");
+
+// LOG TO CONSOLE
+console.log("Hello World");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error("This is some error");
+console.clear();
+console.warn("this is a warning");
+console.time("Hello");
+console.log("Hello World");
+console.timeEnd("Hello");
