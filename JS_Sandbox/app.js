01441390b8745dae7ee425eb46d34ff8e96ac88b
alexangria1992 <alex040892@gmail.com>
2022-07-16 18:50:12 +1200

    var, let & const

--- JS_Sandbox/app.js
+++ JS_Sandbox/app.js
@@ -1,18 +1,68 @@
 // alert("hello");
 
 // LOG TO CONSOLE
-console.log("Hello World");
-console.log(123);
-console.log(true);
-var greeting = "Hello";
+
+// console.log("Hello World");
+// console.log(123);
+// console.log(true);
+// var greeting = "Hello";
+// console.log(greeting);
+
+// console.log([1, 2, 3, 4]);
+// console.log({ a: 1, b: 2 });
+// console.table({ a: 1, b: 2 });
+// console.error("This is some error");
+// console.clear();
+// console.warn("this is a warning");
+// console.time("Hello");
+// console.log("Hello World");
+// console.timeEnd("Hello");
+
+// Var, let Const
+
+var name = "John Doe";
+console.log(name);
+name = "Steve Smith";
+console.log(name);
+
+// // init var
+var greeting;
+console.log(greeting);
+greeting = "Hello";
 console.log(greeting);
 
-console.log([1, 2, 3, 4]);
-console.log({ a: 1, b: 2 });
-console.table({ a: 1, b: 2 });
-console.error("This is some error");
-console.clear();
-console.warn("this is a warning");
-console.time("Hello");
-console.log("Hello World");
-console.timeEnd("Hello");
+// //multi word var
+
+var firstName = "John"; // Camel Case
+var first_name = "Sarah"; // Underscore method
+var FirstName = "Tom" // Pascal case
+var firstname;
+
+// LET
+
+let name;
+name = "John Doe";
+console.log(name);
+name = "Steve Smith";
+console.log(name);
+
+// CONST
+
+const name = "John";
+console.log(name);
+// Can not reassign
+name = "Sarah";
+Have to assign a value;
+const greeting;
+
+const person = {
+  name: "John",
+  age: 30,
+};
+
+person.name = "Sarah";
+person.age = 32;
+//
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
